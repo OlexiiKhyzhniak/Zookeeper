c31f905c95a538f073c17ff2d81d40a39ea3a2dc
Olexii Khyzhniak <63338851+OlexiiKhyzhniak@users.noreply.github.com>
2021-03-01 21:15:27 +0200

    Initial commit

---
+++ Problems/Favor/main.py
@@ -0,0 +1,7 @@
+k = int(input())
+i = 1
+sumchi = 1
+while i < k:
+    i += 1
+    sumchi += i
+print(sumchi)
---
+++ Problems/Find even/main.py
@@ -0,0 +1,6 @@
+k = int(input())
+i = 0
+while i < (k - 1):
+    i += 1
+    if i % 2 == 0:
+        print(i)
